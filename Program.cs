e2794c58abbe6bb02f62ab2f815b5f8de9f81ad0
Kaio <kaio.santiago.13@hotmail.com>
2023-10-15 16:01:53 -0300

    padronizando

--- Program.cs
+++ Program.cs
@@ -5,28 +5,45 @@ var app = builder.Build();
 
 List<Product> lista = new List<Product>();
 
-app.MapPost("/save", (Product prod) => {
-    lista.Add(prod);
-    return prod.Code + " - " + prod.Name;
+app.MapPost("/products", ([FromBody]Product prod) => { 
+    bool equal = false;   
+    foreach (var item in lista)
+    {
+        if(item.Code == prod.Code && item.Name == item.Name)
+            equal = true;
+    }
+    if(equal == false || lista.Count == 0)
+    {
+        lista.Add(prod);
+        return Results.Created("/product" + prod.Code, prod.Code);   
+    } 
+    else
+    {
+        return Results.StatusCode(409);
+    }  
 });
-app.MapPost("/deleteElement/{index}", ([FromRoute]string index) => {
-    lista.RemoveAt(Convert.ToInt32(index));
+app.MapDelete("/products/{code}", ([FromRoute]string code) => {
+    lista.RemoveAll(item => item.Code == code);
 });
-app.MapGet("/getLenList", () => {
-    return lista.Count;
-});
-app.MapGet("/getList", () => {
+app.MapGet("/products", () => {
     return lista;
 });
-app.MapGet("/getElement/{index}", ([FromRoute]string index) => {
-    try
+app.MapPut("/products", (Product prod) =>{
+    bool found = false;
+    foreach (var item in lista)
     {
-        return lista[Convert.ToInt32(index)];
+        if(found == true)
+            break;
+
+        if(item.Code.ToLower().Equals(prod.Code.ToLower()))
+        {
+            item.Code = prod.Code;
+            item.Name = prod.Name;
+            found = true;
+        }
     }
-    catch (System.Exception ex)
-    {
-        throw new Exception($"{ex.Message}");
-    }
+    if(found == false)
+        lista.Add(prod);
 });
 
 app.Run();
